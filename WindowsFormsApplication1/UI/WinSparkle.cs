30a22e7eddbeca5b6cbccabb42e6735d9c441fa9
Ellendar <ellendar@gmail.com>
2023-04-13 00:54:52 -0400

    Fixed a bug manually setting volcano biome (for real this time). Testing release candidate for 4.1.0

--- WindowsFormsApplication1/UI/WinSparkle.cs
+++ WindowsFormsApplication1/UI/WinSparkle.cs
@@ -5,25 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 
-namespace Z2Randomizer.UI
-{
+namespace Z2Randomizer.UI;
 
-    class WinSparkle
-    {
-        // Note that some of these functions are not implemented by WinSparkle YET.
-        [DllImport("WinSparkle.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void win_sparkle_init();
-        [DllImport("WinSparkle.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void win_sparkle_cleanup();
-        [DllImport("WinSparkle.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void win_sparkle_set_appcast_url(String url);
-        [DllImport("WinSparkle.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void win_sparkle_set_app_details(String company_name,
-                                             String app_name,
-                                             String app_version);
-        [DllImport("WinSparkle.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void win_sparkle_set_registry_path(String path);
-        [DllImport("WinSparkle.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void win_sparkle_check_update_with_ui();
-    }
+
+class WinSparkle
+{
+    public const string DLL_NAME = "WinSparkle_0_8_0_x64.dll";
+    // Note that some of these functions are not implemented by WinSparkle YET.
+    [DllImport(DLL_NAME, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void win_sparkle_init();
+    [DllImport(DLL_NAME, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void win_sparkle_cleanup();
+    [DllImport(DLL_NAME, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void win_sparkle_set_appcast_url(String url);
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void win_sparkle_set_app_details(String company_name,
+                                         String app_name,
+                                         String app_version);
+    [DllImport(DLL_NAME, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void win_sparkle_set_registry_path(String path);
+    [DllImport(DLL_NAME, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void win_sparkle_check_update_with_ui();
 }
