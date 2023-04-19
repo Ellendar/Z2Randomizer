c80c9c50024b8c5df1cc2f280056d3bf7248bad6
James Rowe <jroweboy@gmail.com>
2023-04-19 14:17:49 -0400

    Migrate to using a core randomizer dll and a separate ui project

---
+++ WinFormUI/Program.cs
@@ -0,0 +1,18 @@
+using Z2Randomizer;
+
+namespace WinFormUI
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainUI());
+        }
+    }
+}
