e75aef2453e23e4874d2ad9defe7ca5999fe195f
James Rowe <jroweboy@gmail.com>
2024-01-03 17:11:30 -0500

    Prevent wizrobes from using new beams and remove usages of 1

--- RandomizerCore/ROM.cs
+++ RandomizerCore/ROM.cs
@@ -840,6 +840,20 @@ CheckController1ForUpAMagic:
         var a = _engine.Asm();
         a.code("""
 .segment "PRG4"
+
+.define ProjectileEnemyData $bc
+.define ProjectileYPosition $30
+.define ProjectileXVelocity $77
+.define EnemyType           $a1
+.define LinkXPosition       $4d
+.define EnemyXPositionLo    $4e
+.define EnemyXPositionHi    $3c
+.define RNG                 $051b
+.define SinWaveExtra        $07c0
+
+EnemyYVelocity = $057e
+; SinWaveVelocityIncrement = $ba1c
+
 ; Hook into carrock's update code to add the new position calculation
 .org $aec4
     jmp ChooseNewCarrockXPosition
@@ -851,45 +865,36 @@ ChooseNewCarrockXPosition:
     ; roll a random number and force carrock to only appear on the opposite side
     ; remove the MSB from the random value and we will replace it with the opposite
     ; bit from Link's MSB
-    lda $051b,x
+    lda RNG,x
     ; By shifting links opposite side bit twice, we can force Carrock to be closer to the wall
     asl
-    ;asl
-    sta $4e,x
+    sta EnemyXPositionLo,x
 
     ; Since Link's position can be from roughly $00fb - 01ef
     ; We can offset this by flipping the high bit, and then checking if its greater than
     ; $80 - $10 (which would be like starting the range from F0 - 70)
-    lda $4d
+    lda LinkXPosition
     eor #$80
     cmp #$70
 
     ; At this point, the carry contains the bit for the opposite side of the screen for link
     ; So shift it back into bosses position
-    ror $4e,x
-    ;cmp #$70
-    ;ror $4e,x
+    ror EnemyXPositionLo,x
 
     ; Vanilla checks to see if carrock is too close to the right edge, and prevents going
     ; offscreen by dividing the position by two if it would
     lda #$e0
-    cmp $4e,x
+    cmp EnemyXPositionLo,x
     bcs @SetXHighPos
         sbc #$10 - 1
-        sta $4e,x
+        sta EnemyXPositionLo,x
 @SetXHighPos:
     lda #1
-    sta $3c,x
+    sta EnemyXPositionHi,x
     rts
 
 .org $996f
     jsr MoveSinWaveWifiShot
-
-.define ProjectileEnemyData $bc
-.define ProjectileYPosition $30
-.define ProjectileXVelocity $77
-EnemyYVelocity = $057e
-; SinWaveVelocityIncrement = $ba1c
 
 .reloc
 SinWaveVelocityIncrement:
@@ -898,13 +903,10 @@ SinWaveMaxVelocityExtant:
     .byte $28, $d4
 
 MoveSinWaveWifiShot:
-    ; Wifi shot id from carrock is $9 (and this number is premultiplied by 2)
-    cmp #$12
-    bne @NotAWifiShotFromCarrock
     ; Now check to see if we set the flag for sin waves.
-    ; We write b0 to 71,x if this wifi is a sin wave so we can check the high bit if its set then its a sin wav
-    lda $71,x
-    bpl @NotAWifiShotFromCarrock
+    ; We write b0 to SinWaveExtra,x if this wifi is a sin wave so we can check the high bit if its set then its a sin wav
+    lda SinWaveExtra,x
+    bpl @NotAWifiShot
         lda ProjectileEnemyData,x
         and #$01
         tay
@@ -927,7 +929,7 @@ MoveSinWaveWifiShot:
         adc ProjectileYPosition,x
         sta ProjectileYPosition,x
     ldy #$12
-@NotAWifiShotFromCarrock:
+@NotAWifiShot:
     ; Perform the original patched call
     lda $6ec0,y
     rts
@@ -937,10 +939,19 @@ MoveSinWaveWifiShot:
 
 .reloc
 RandomizeWifiShotType:
+    ldx $10
+    lda EnemyType,x
+    cmp #$22 ; check if its spawned by carock and skip it. carock code id is 0x22
+    beq @CarockShot
+        ; Do the original code since its not our new buffed carock
+        lda #$c0
+        sta ProjectileYPosition,y
+        rts
+@CarockShot:
     ; Fetch a random number and 50/50 chance its a straight shot
-    bit $051c
+    lda #0
+    bit RNG+1
     bmi @StraightShot
-        lda #0
         bvc @HiPositionShot
             ; Shooting from the bottom should start by arcing upwards
             lda #1
@@ -950,11 +961,10 @@ RandomizeWifiShotType:
         sta EnemyYVelocity,y
         lda #$b0
         ; Firing a sin wave shot, so set the flag for sin wave and then follow through setting up the position
-        sta $0071,y
+        sta SinWaveExtra,y
         bne @WriteInitCoord
 @StraightShot:
-    lda #0
-    sta $0071,y
+    sta SinWaveExtra,y
     lda #$b0
     ; we are shooting straight so clear out the flag
     ; 50/50 chance that the shot starts high or low
@@ -963,16 +973,16 @@ RandomizeWifiShotType:
         clc
         adc #$10
 @WriteShotYCoord:
-    sta $0030,y
+    sta ProjectileYPosition,y
     ; roll a random number to see if we shoot a fast beam
-    lda $051c
+    lda RNG+1
     and #1
     beq @Exit
         lda ProjectileXVelocity,y
         bmi @NegativeVelocity
            clc
            adc #$10
-           bne @DoneVelocityUpdate
+           bne @DoneVelocityUpdate ; unconditional
 @NegativeVelocity:
         sec
         sbc #$10
