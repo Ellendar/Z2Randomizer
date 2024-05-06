4024ac8efb45808487319530a87d992ef02da9c7
James Rowe <jroweboy@gmail.com>
2024-05-05 21:09:22 -0400

    Add roll seed button

--- CrossPlatformUI/ViewModels/HeaderViewModel.cs
+++ CrossPlatformUI/ViewModels/HeaderViewModel.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Reactive;
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
 using Avalonia.Data;
 using ReactiveUI;
 using ReactiveUI.Validation.Helpers;
@@ -14,10 +18,17 @@ public class HeaderViewModel : ReactiveValidationObject, IRoutableViewModel
     {
         config = mainViewModel.Config;
         HostScreen = mainViewModel;
+        
+        RerollSeed = ReactiveCommand.Create(() =>
+        {
+            Seed = new Random().Next(0, 999999999).ToString();
+        });
     }
-    
+
+    public ReactiveCommand<Unit, Unit> RerollSeed { get; }
+
     [DataMember]
-    public string Flags
+    public string? Flags
     {
         get => config.Serialize();
         set => this.ValueOrException(ref config, () => new RandomizerConfiguration(value), "Invalid Flags");
@@ -31,23 +42,23 @@ public class HeaderViewModel : ReactiveValidationObject, IRoutableViewModel
     }
 
     private RandomizerConfiguration config;
-    private string seed;
+    private string seed = "";
     
     [IgnoreDataMember]
     // Unique identifier for the routable view model.
-    public string UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
+    public string UrlPathSegment { get; } = Guid.NewGuid().ToString()[..5];
     [IgnoreDataMember]
     public IScreen HostScreen { get; }
-    
 }
 
 internal static class Extension {
-    public static void ValueOrException<TParent, TBacking>(this TParent parent, ref TBacking backing, Func<TBacking> newValue, string? message = null)
+    public static void ValueOrException<TParent, TBacking>(this TParent parent, ref TBacking backing,
+        Func<TBacking> newValue, string? message = null, [CallerMemberName] string? field = "")
         where TParent : IReactiveObject
     {
         try
         {
-            parent.RaiseAndSetIfChanged(ref backing, newValue.Invoke());
+            parent.RaiseAndSetIfChanged(ref backing, newValue.Invoke(), field);
         }
         catch (Exception e)
         {
