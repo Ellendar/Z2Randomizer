ff129ce8a3210cb46c1f1d2cc5c26194fb3b4813
initsu <init.public@gmail.com>
2025-03-10 02:29:22 +0100

    Add tool tip for Attack Effectiveness
    This is a suggestion - do we want to implement tool tips like this?

---
+++ RandomizerCore/ToolTips.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace RandomizerCore;
+
+public static class ToolTips
+{
+    public static String AttackEffectiveness =
+"""
+Determines how much damage you do to enemies. Each level is randomized individually, but it will never decrease as you level up.
+
+Vanilla: Same attack damage as in the original game.
+
+Low Attack: 50% of vanilla damage (with at least one damage per attack level).
+
+Randomize (Low): Damage for each attack level is randomized between Low Attack and Vanilla.
+
+Randomize: Damage for each attack level is randomized from 66.7% to 150% of vanilla damage.
+
+Randomize (High): Damage for each attack level is randomized between Vanilla and High Attack.
+
+High Attack: 150% of vanilla damage.
+
+Instant Kill: Every enemy dies in one hit.
+""";
+}
