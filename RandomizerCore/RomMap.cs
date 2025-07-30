11240181607125b90a689ac237d49c7106b01425
initsu <init.public@gmail.com>
2025-07-30 22:40:13 +0200

    Refactor Overworld collectable addresses to RomMap

--- RandomizerCore/RomMap.cs
+++ RandomizerCore/RomMap.cs
@@ -7,7 +7,7 @@
 /// </summary>
 class RomMap
 {
-    //Has item addresses
+    //Start with item bytes
     public const int START_CANDLE = 0x17B01;
     public const int START_GLOVE = 0x17B02;
     public const int START_RAFT = 0x17B03;
@@ -17,11 +17,32 @@ class RomMap
     public const int START_HAMMER = 0x17B07;
     public const int START_MAGICAL_KEY = 0x17B08;
 
-    //Start of memory for vanilla locations
+    //Addresses for vanilla locations. These addresses holds Y coordinate
+    //bytes. The X coord bytes are at the same address + 0x3F.
     public const int VANILLA_DESERT_TILE_LOCATION = 0x8649;
 
     //Overworld data
     public const int overworldXOffset = 0x3F;
     public const int overworldMapOffset = 0x7E;
     public const int overworldWorldOffset = 0xBD;
+
+    //Vanilla non-palace collectable ID bytes
+    public const int WEST_GRASS_TILE_COLLECTABLE = 0x4dd7;
+    public const int WEST_TROPHY_CAVE_COLLECTABLE = 0x4dea;
+    public const int WEST_PBAG_CAVE_COLLECTABLE = 0x4fe2;
+    public const int WEST_HEART_CONTAINER_CAVE_COLLECTABLE = 0x4ff5;
+    public const int WEST_MAGIC_CONTAINER_CAVE_COLLECTABLE = 0x502a;
+    public const int WEST_MEDICINE_CAVE_COLLECTABLE = 0x5069;
+    public const int DM_HAMMER_CAVE_COLLECTABLE = 0x6512;
+    public const int DM_SPECTACLE_ROCK_COLLECTABLE = 0x65c3;
+    public const int EAST_PBAG_CAVE1_COLLECTABLE = 0x8ecc;
+    public const int EAST_WATER_TILE_COLLECTABLE = 0x8faa;
+    public const int EAST_PBAG_CAVE2_COLLECTABLE = 0x8fb3;
+    public const int EAST_DESERT_TILE_COLLECTABLE = 0x9011;
+    public const int EAST_NEW_KASUTO_BASEMENT_COLLECTABLE = 0xDb8c;
+    public const int EAST_SPELL_TOWER_COLLECTABLE = 0xDb95;
+    public const int MI_CHILD_DROP_COLLECTABLE = 0xA58b;
+    public const int MI_MAGIC_CONTAINER_DROP_COLLECTABLE = 0xA5a8;
+    // int[] VANILLA_PALACE_COLLECTABLE_BYTES = [0x10E91, 0x10E9A, 0x1252D, 0x12538, 0x10EA3, 0x12774];
+
 }
