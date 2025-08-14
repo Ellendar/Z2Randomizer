1061270a6543bb840a6c0f02ad1fed4909262c15
initsu <init.public@gmail.com>
2025-08-14 17:04:55 +0200

    Refactor flag presets into their own files
    Mostly to easier diff the presets and keep it manageable

---
+++ CrossPlatformUI/Presets/MaxRandoPreset.cs
@@ -0,0 +1,126 @@
+﻿using Z2Randomizer.RandomizerCore;
+using Z2Randomizer.RandomizerCore.Overworld;
+
+namespace CrossPlatformUI.Presets;
+
+public static class MaxRandoPreset
+{
+    public static readonly RandomizerConfiguration Preset = new()
+    {
+        //Start
+        ShuffleStartingItems = true,
+        ShuffleStartingSpells = true,
+        MaxHeartContainers = MaxHeartsOption.RANDOM,
+        StartingHeartContainersMin = 1,
+        StartingHeartContainersMax = 8,
+        StartingTechniques = StartingTechs.NONE,
+        StartingLives = StartingLives.LivesRandom,
+        IndeterminateOptionRate = IndeterminateOptionRate.HALF,
+
+        //Overworld
+        PalacesCanSwapContinents = true,
+        ShuffleGP = true,
+        ShuffleEncounters = true,
+        AllowUnsafePathEncounters = true,
+        IncludeLavaInEncounterShuffle = true,
+        EncounterRate = EncounterRate.HALF,
+        EastRocks = false,
+        GenerateBaguWoods = false,
+        HideLessImportantLocations = true,
+        RestrictConnectionCaveShuffle = true,
+        AllowConnectionCavesToBeBlocked = true,
+        GoodBoots = true,
+        HidePalace = null,
+        HideKasuto = null,
+        ShuffleWhichLocationIsHidden = true,
+        ContinentConnectionType = ContinentConnectionType.ANYTHING_GOES,
+
+        Climate = Climates.Classic,
+        WestBiome = Biome.RANDOM_NO_VANILLA,
+        EastBiome = Biome.RANDOM_NO_VANILLA,
+        MazeBiome = Biome.VANILLALIKE,
+        DMBiome = Biome.RANDOM_NO_VANILLA,
+        VanillaShuffleUsesActualTerrain = true,
+
+        //Palaces
+        NormalPalaceStyle = PalaceStyle.RECONSTRUCTED,
+        GPStyle = PalaceStyle.RECONSTRUCTED,
+        ShortenNormalPalaces = false,
+        ShortenGP = null,
+        IncludeVanillaRooms = true,
+        Includev4_0Rooms = true,
+        Includev4_4Rooms = false,
+        TBirdRequired = false,
+        RemoveTBird = false,
+        PalacesToCompleteMin = 0,
+        PalacesToCompleteMax = 6,
+        RestartAtPalacesOnGameOver = true,
+        ChangePalacePallettes = true,
+        RandomizeBossItemDrop = true,
+        BossRoomsExitType = BossRoomsExitType.RANDOM_PER_PALACE,
+        NoDuplicateRoomsByLayout = false,
+        BlockingRoomsInAnyPalace = true,
+        HardBosses = true,
+        PalaceItemRoomCount = PalaceItemRoomCount.ONE,
+        DarkLinkMinDistance = BossRoomMinDistance.NONE,
+
+        //Levels
+        ShuffleAttackExperience = true,
+        ShuffleMagicExperience = true,
+        ShuffleLifeExperience = true,
+
+        AttackLevelCap = 8,
+        MagicLevelCap = 8,
+        LifeLevelCap = 8,
+        AttackEffectiveness = AttackEffectiveness.AVERAGE,
+        MagicEffectiveness = MagicEffectiveness.AVERAGE,
+        LifeEffectiveness = LifeEffectiveness.AVERAGE,
+
+        //Spells
+        ShuffleLifeRefillAmount = true,
+        ShuffleSpellLocations = true,
+        DisableMagicContainerRequirements = true,
+        RandomizeSpellSpellEnemy = true,
+        SwapUpAndDownStab = false,
+        FireOption = FireOption.RANDOM,
+
+        //Enemies
+        ShuffleOverworldEnemies = true,
+        ShufflePalaceEnemies = true,
+        ShuffleDripperEnemy = true,
+        MixLargeAndSmallEnemies = true,
+        GeneratorsAlwaysMatch = true,
+
+        ShuffleEnemyHP = true,
+        ShuffleXPStealers = true,
+        ShuffleXPStolenAmount = true,
+        ShuffleSwordImmunity = true,
+        EnemyXPDrops = XPEffectiveness.RANDOM,
+
+        //Items
+        ShufflePalaceItems = true,
+        ShuffleOverworldItems = true,
+        MixOverworldAndPalaceItems = true,
+        IncludePBagCavesInItemShuffle = true,
+        IncludeSwordTechsInShuffle = false,
+        IncludeQuestItemsInShuffle = false,
+        IncludeSpellsInShuffle = false,
+
+        ShuffleSmallItems = true,
+        RemoveSpellItems = false,
+        ShufflePBagAmounts = true,
+        PalacesContainExtraKeys = false,
+        RandomizeNewKasutoJarRequirements = true,
+        AllowImportantItemDuplicates = false,
+
+        //Drops
+        ShuffleItemDropFrequency = true,
+        RandomizeDrops = true,
+        StandardizeDrops = true,
+
+        //Hints
+        EnableHelpfulHints = true,
+        EnableSpellItemHints = true,
+        EnableTownNameHints = true
+    };
+}
