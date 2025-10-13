dc392905cf46916e8b928c0bfd800d01efcfeb9d
initsu <init.public@gmail.com>
2025-10-14 01:50:51 +0200

    Improve naming of some of the flags in the UI
    - Include Quest Items => Include Town Quest Items in Shuffle   (could easily have meant the vanilla items like Trophy, etc.)
    - Hide Palace/Kasuto => Generate Three-Eyed Rock, Generate Hidden Kasuto Tile
    
    Also "Shuffle Which Locations Are Hidden" is now grayed out when not relevant.

--- CrossPlatformUI/Views/Tabs/OverworldView.axaml.cs
+++ CrossPlatformUI/Views/Tabs/OverworldView.axaml.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Reactive.Linq;
+using System.Reactive.Disposables;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using CrossPlatformUI.ViewModels;
 using ReactiveUI;
+using Avalonia;
 
 namespace CrossPlatformUI.Views.Tabs;
 
@@ -9,8 +14,19 @@ public partial class OverworldView : ReactiveUserControl<MainViewModel>
 {
     public OverworldView()
     {
-        this.WhenActivated(disposables => { });
         AvaloniaXamlLoader.Load(this);
-    }
+        this.WhenActivated(disposables =>
+        {
+            CheckBox generateThreeEyedRock = this.FindControl<CheckBox>("GenerateThreeEyedRock") ?? throw new System.Exception("Missing Required Validation Element");
+            CheckBox generateHiddenKasutoTile = this.FindControl<CheckBox>("GenerateHiddenKasutoTile") ?? throw new System.Exception("Missing Required Validation Element");
+            CheckBox shuffleWhichLocationsAreHidden = this.FindControl<CheckBox>("ShuffleWhichLocationsAreHidden") ?? throw new System.Exception("Missing Required Validation Element");
 
+            var rockObservable = generateThreeEyedRock.GetObservable(CheckBox.IsCheckedProperty);
+            var forestObservable = generateHiddenKasutoTile.GetObservable(CheckBox.IsCheckedProperty);
+
+            rockObservable.CombineLatest(forestObservable, (rock, forest) => (rock ?? true) || (forest ?? true))
+                .Subscribe(possibleHiddenEastTile => { shuffleWhichLocationsAreHidden.IsEnabled = possibleHiddenEastTile; })
+                .DisposeWith(disposables);
+        });
+    }
 }
