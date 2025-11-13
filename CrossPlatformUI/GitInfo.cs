7af2a453510c8186d0154f1b1c2c6decdb880f1f
initsu <init.public@gmail.com>
2025-11-13 12:04:54 +0100

    Fix for Visual Studio not finding GitInfo.cs #2

---
+++ CrossPlatformUI/GitInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Z2Randomizer;
+
+public static partial class GitInfo
+{
+    public static partial string Commit { get; }
+    public static partial string Branch { get; }
+    public static partial bool IsDirty { get; }
+}
