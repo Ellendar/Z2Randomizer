8d86e8493c2632c9e550e77e5ccdbc5370ee25f2
Patrick McNeill <patrick@mcneill.one>
2026-04-25 18:05:42 -0400

    Removes wetlands from random climate pool

--- RandomizerCore/RandomizerConfiguration.cs
+++ RandomizerCore/RandomizerConfiguration.cs
@@ -1222,13 +1222,12 @@ public sealed partial class RandomizerConfiguration : INotifyPropertyChanged
 
         if (westClimate == ClimateEnum.RANDOM)
         {
-            properties.WestClimate = r.Next(5) switch
+            properties.WestClimate = r.Next(4) switch
             {
                 0 => ClimateEnum.CLASSIC,
                 1 => ClimateEnum.CHAOS,
-                2 => ClimateEnum.WETLANDS,
-                3 => ClimateEnum.GREAT_LAKES,
-                4 => ClimateEnum.SCRUBLAND,
+                2 => ClimateEnum.GREAT_LAKES,
+                3 => ClimateEnum.SCRUBLAND,
                 _ => throw new Exception("Unrecognized climate")
             };
         }
@@ -1238,13 +1237,12 @@ public sealed partial class RandomizerConfiguration : INotifyPropertyChanged
         }
         if (eastClimate == ClimateEnum.RANDOM)
         {
-            properties.EastClimate = r.Next(5) switch
+            properties.EastClimate = r.Next(4) switch
             {
                 0 => ClimateEnum.CLASSIC,
                 1 => ClimateEnum.CHAOS,
-                2 => ClimateEnum.WETLANDS,
-                3 => ClimateEnum.GREAT_LAKES,
-                4 => ClimateEnum.SCRUBLAND,
+                2 => ClimateEnum.GREAT_LAKES,
+                3 => ClimateEnum.SCRUBLAND,
                 _ => throw new Exception("Unrecognized climate")
             };
         }
@@ -1254,13 +1252,12 @@ public sealed partial class RandomizerConfiguration : INotifyPropertyChanged
         }
         if (dmClimate == ClimateEnum.RANDOM)
         {
-            properties.DmClimate = r.Next(5) switch
+            properties.DmClimate = r.Next(4) switch
             {
                 0 => ClimateEnum.CLASSIC,
                 1 => ClimateEnum.CHAOS,
-                2 => ClimateEnum.WETLANDS,
-                3 => ClimateEnum.GREAT_LAKES,
-                4 => ClimateEnum.DM_SCRUBLAND,
+                2 => ClimateEnum.GREAT_LAKES,
+                3 => ClimateEnum.DM_SCRUBLAND,
                 _ => throw new Exception("Unrecognized climate")
             };
         }
