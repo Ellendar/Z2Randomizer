3fad1bbbc3a84e365a3ee0b8dd62e92ae0aa310f
James Rowe <jroweboy@gmail.com>
2026-06-17 10:53:11 -0400

    Remove any fields from the presets that are vanilla and add vanilla preset

---
+++ CrossPlatformUI/Presets/VanillaPreset.cs
@@ -0,0 +1,11 @@
+﻿using Z2Randomizer.RandomizerCore;
+
+namespace CrossPlatformUI.Presets;
+
+/// <summary>
+/// Uses all the default values, which should always be vanilla.
+/// </summary>
+public static class VanillaPreset
+{
+    public static readonly RandomizerConfiguration Preset = new();
+}
