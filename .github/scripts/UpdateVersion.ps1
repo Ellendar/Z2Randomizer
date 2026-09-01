7601745907da09b9c013e55e6e62f7c894cb36da
Ellendar <ellendar@gmail.com>
2026-09-01 09:28:51 -0400

    Update UpdateVersion.ps1

--- .github/scripts/UpdateVersion.ps1
+++ .github/scripts/UpdateVersion.ps1
@@ -44,7 +44,7 @@ $file = "README.md"
     } | Set-Content $file
 git add $file
 
-$file = "Z2RandomizerSetup/Setup1.vdproj"
+$file = "Z2RandomizerSetup/Z2RandomizerSetup.vdproj"
 $newProductCode = [guid]::NewGuid().ToString("B").ToUpper()
 $newPackageCode = [guid]::NewGuid().ToString("B").ToUpper()
 $content = Get-Content $file
